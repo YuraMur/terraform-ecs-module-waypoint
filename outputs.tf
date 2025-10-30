ae2bd67927abf226ea0e393aea8de0ec796e4499
Yuriy Murdza <yuramurdza@gmail.com>
2025-10-30 15:20:10 +0200

    ecs cluster

--- outputs.tf
+++ outputs.tf
@@ -6,26 +6,6 @@ output "region" {
   value       = var.region
 }
 
-output "rds_hostname" {
-  description = "RDS instance hostname."
-  value       = aws_db_instance.education.address
+output "ecs-cluster-name" {
+  value = aws_ecs_cluster.cluster.name
 }
-
-output "rds_port" {
-  description = "RDS instance port."
-  value       = aws_db_instance.education.port
-  sensitive   = true
-}
-
-output "rds_dbname" {
-  description = "RDS instance database name."
-  value       = var.db_name
-  sensitive   = true
-}
-
-output "rds_username" {
-  description = "RDS instance root username."
-  value       = aws_db_instance.education.username
-  sensitive   = true
-}
-
--- variables.tf
+++ variables.tf
@@ -4,15 +4,11 @@
 variable "region" {
   description = "AWS region for all resources."
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
-variable "db_name" {
-  description = "Unique name to assign to RDS instance."
+variable "name" {
+  description = "Resource name prefix"
   type        = string
-}
-
-variable "db_username" {
-  description = "RDS root username."
-  type        = string
+  default     = "default"
 }
